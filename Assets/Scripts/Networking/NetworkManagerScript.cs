34100957eb936c29c31877bdc1e095bdff64eeab
SakuretsuKumare <Dawncoming3@gmail.com>
2023-03-28 10:44:03 -0400

    Added Network EventCodes
    Raises events on what happened and we can do things based off of which event you want to be called.

--- Assets/Scripts/Networking/NetworkManagerScript.cs
+++ Assets/Scripts/Networking/NetworkManagerScript.cs
@@ -5,6 +5,7 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.SceneManagement;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
+using ExitGames.Client.Photon;
 
 /* Code was referenced from https://www.youtube.com/watch?v=KHWuTBmT1oI
  * https://www.youtube.com/watch?v=zPZK7C5_BQo&list=PLhsVv9Uw1WzjI8fEBjBQpTyXNZ6Yp1ZLw */
@@ -25,6 +26,8 @@ public class NetworkManagerScript : MonoBehaviourPunCallbacks
     [SerializeField, Tooltip("Name of network player prefab in Resources folder.")]     private string networkPlayerName;
     [SerializeField]                                                                    private string readyUpManagerName = "ReadyUpManager";
     [SerializeField, Tooltip("Allow use of some of the worse words in our vocabulary")] private bool useFunnyWords;
+    // Event code to use when a player leaves the room
+    private const byte PLAYER_LEFT_EVENT = 1;
 
     [SerializeField] private WordStructure[] wormAdjectives = { new WordStructure("Unfortunate", new int[4]), new WordStructure("Sad", new int[4]), new WordStructure("Despairing", new int[4]), new WordStructure("Grotesque", new int[4]), new WordStructure("Despicable", new int[4]), new WordStructure("Abhorrent", new int[4]), new WordStructure("Regrettable", new int[4]), new WordStructure("Incorrigible", new int[4]), new WordStructure("Greasy", new int[4]), new WordStructure("Platonic", new int[4]), new WordStructure("Sinister", new int[4]), new WordStructure("Hideous", new int[4]), new WordStructure("Glum", new int[4]), new WordStructure("Blasphemous", new int[4]), new WordStructure("Malignant", new int[4]), new WordStructure("Undulating", new int[4]), new WordStructure("Treacherous", new int[4]), new WordStructure("Hostile", new int[4]), new WordStructure("Slimy", new int[4]), new WordStructure("Squirming", new int[4]), new WordStructure("Blubbering", new int[4]), new WordStructure("Twisted", new int[4]), new WordStructure("Manic", new int[4]), new WordStructure("Slippery", new int[4]), new WordStructure("Wet", new int[4]), new WordStructure("Moist", new int[4]), new WordStructure("Lugubrious", new int[4]), new WordStructure("Tubular", new int[4]), new WordStructure("Little", new int[4]), new WordStructure("Erratic", new int[4]), new WordStructure("Pathetic", new int[4]) };
     [SerializeField] private WordStructure[] wormNouns = { new WordStructure("Invertebrate", new int[4]), new WordStructure("Wormlet", new int[4]), new WordStructure("Creature", new int[4]), new WordStructure("Critter", new int[4]), new WordStructure("Fool", new int[4]), new WordStructure("Goon", new int[4]), new WordStructure("Specimen", new int[4]), new WordStructure("Homonculus", new int[4]), new WordStructure("Grubling", new int[4]), new WordStructure("Snotling", new int[4]), new WordStructure("Wormling", new int[4]), new WordStructure("Nightcrawler", new int[4]), new WordStructure("Stinker", new int[4]), new WordStructure("Rapscallion", new int[4]), new WordStructure("Scalliwag", new int[4]), new WordStructure("Beastling", new int[4]), new WordStructure("Crawler", new int[4]), new WordStructure("Larva", new int[4]), new WordStructure("Dingus", new int[4]), new WordStructure("Freak", new int[4]), new WordStructure("Blighter", new int[4]), new WordStructure("Cretin", new int[4]), new WordStructure("Dink", new int[4]), new WordStructure("Unit", new int[4]), new WordStructure("Denizen", new int[4]), new WordStructure("Parasite", new int[4]), new WordStructure("Organism", new int[4]), new WordStructure("Worm", new int[4]), new WordStructure("Oonge", new int[4]), new WordStructure("Bwarp", new int[4]) };
@@ -51,6 +54,8 @@ public class NetworkManagerScript : MonoBehaviourPunCallbacks
     {
         RefreshWormNames();             //Generates the list of potential worm names
         GenerateRandomNickname(true);   //Generates a random nickname on start
+
+        PhotonNetwork.AddCallbackTarget(this);
 
         if (FindObjectOfType<AutoJoinRoom>() != null)
             ConnectAndGiveDavidYourIPAddress(); //Immediately start trying to connect to master server
@@ -343,7 +348,26 @@ public class NetworkManagerScript : MonoBehaviourPunCallbacks
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
         base.OnPlayerLeftRoom(otherPlayer);
-        Debug.Log(otherPlayer.NickName + " has left.");
+        Debug.Log(otherPlayer.NickName + " has left or disconnected.");
+
+        PhotonNetwork.RaiseEvent(1, otherPlayer.ActorNumber, RaiseEventOptions.Default, SendOptions.SendReliable);
+    }
+
+    // This method is called when a custom event is received
+    public void OnEvent(byte eventCode, object content, int senderId)
+    {
+        if (eventCode == 1)
+        {
+            int actorNumber = (int)content;
+
+            // Do something with the actorNumber of the player who left
+
+            // Updates the ReadyUpManager
+            if (ReadyUpManager.instance != null)
+            {
+                ReadyUpManager.instance.UpdateStatus(ReadyUpManager.instance.localPlayerTube.GetTubeNumber());
+            }
+        }
     }
 
     public override void OnLeftRoom()
