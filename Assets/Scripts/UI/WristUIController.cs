6a78105f53f13083e691528faa95a0b1dedfd213
pgomesProjects <70992905+pgomesProjects@users.noreply.github.com>
2023-03-29 13:08:07 -0400

    Wrist Menu Disconnects From Server

--- Assets/Scripts/UI/WristUIController.cs
+++ Assets/Scripts/UI/WristUIController.cs
@@ -88,6 +88,7 @@ public class WristUIController : MonoBehaviour
     {
         PhotonNetwork.LeaveRoom();  //Leave the room
         PhotonNetwork.LeaveLobby(); //Leave the lobby
+        PhotonNetwork.Disconnect(); //Disconnects from the server
 
         SceneManager.LoadScene(GameSettings.titleScreenScene);    //Go back to the main menu
     }
