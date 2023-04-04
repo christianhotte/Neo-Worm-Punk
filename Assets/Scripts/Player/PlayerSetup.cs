e9d7bbd3b67b9a158493b6e062e375868fcd4dc4
pgomesProjects <70992905+pgomesProjects@users.noreply.github.com>
2023-04-04 11:54:05 -0400

    Fixed An Oopsie

--- Assets/Scripts/Player/PlayerSetup.cs
+++ Assets/Scripts/Player/PlayerSetup.cs
@@ -36,6 +36,7 @@ public class PlayerSetup : MonoBehaviour
 
     public void SetNickname()
     {
-        NetworkManagerScript.localNetworkPlayer.SetWormNicknameText(NetworkManagerScript.instance.GetLocalPlayerName());
+        if(NetworkManagerScript.localNetworkPlayer != null)
+            NetworkManagerScript.localNetworkPlayer.SetWormNicknameText(NetworkManagerScript.instance.GetLocalPlayerName());
     }
 }
