4e920394521f17a3e2d9a61428d1937818c7d1b5
pgomesProjects <70992905+pgomesProjects@users.noreply.github.com>
2023-04-04 11:51:41 -0400

    Canvas Scaler and Percentage Stuff

---
+++ Assets/Scripts/UI/CanvasDistanceScaler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class CanvasDistanceScaler : MonoBehaviour
+{
+    [SerializeField] private float minimumDistance = 10f;
+    [SerializeField] private float maximumDistance = 50f;
+
+    [SerializeField] private float minimumScale = 1f;
+    [SerializeField] private float maximumScale = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentDistance = Mathf.Clamp(Vector3.Distance(transform.position, PlayerController.instance.transform.position), minimumDistance, maximumDistance);
+        float currentScale = GetPercentBetweenRange(currentDistance);
+        transform.localScale = new Vector3(0.01f * currentScale, 0.01f * currentScale, 0.01f * currentScale);
+    }
+
+    private float GetPercentBetweenRange(float currentDistance) => (currentDistance - minimumDistance) / (maximumDistance - minimumDistance) * 100f;
+}
