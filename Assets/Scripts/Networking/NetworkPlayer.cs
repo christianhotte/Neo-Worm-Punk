07f8a7e1964a810bdcb7ed6e8952356e60b3bb26
pgomesProjects <70992905+pgomesProjects@users.noreply.github.com>
2023-04-06 11:16:49 -0400

    Added Back the Deprecated SpawnManagers

--- Assets/Scripts/Networking/NetworkPlayer.cs
+++ Assets/Scripts/Networking/NetworkPlayer.cs
@@ -144,8 +144,6 @@ public class NetworkPlayer : MonoBehaviour
     }
     private void OnDestroy()
     {
-        //photonView.RPC("RPC_TubeVacated", RpcTarget.All, lastTubeNumber);
-
         //Reference cleanup:
         instances.Remove(this);                                                                                 //Remove from instance list
         if (photonView.IsMine && PlayerController.photonView == photonView) PlayerController.photonView = null; //Clear client photonView reference
@@ -217,8 +215,6 @@ public class NetworkPlayer : MonoBehaviour
     private void OnPlayerDisconnected(NetworkPlayer player)
     {
         Debug.Log("Cleaning up after player " + player);
-
-        photonView.RPC("RPC_TubeVacated", RpcTarget.All, lastTubeNumber);
         //RemoveRPCs(player);
         //DestroyPlayerObjects(player);
     }
@@ -283,7 +279,7 @@ public class NetworkPlayer : MonoBehaviour
     /// </summary>
     public void UpdateTakenColorsOnJoin()
     {
-        if(ReadyUpManager.instance != null)
+        if (ReadyUpManager.instance != null)
         {
             List<int> takenColors = new List<int>();
 
@@ -310,7 +306,7 @@ public class NetworkPlayer : MonoBehaviour
             //If the player must replace their color, change their color
             if (mustReplaceColor)
             {
-                for(int i = 0; i < PlayerSettingsController.NumberOfPlayerColors(); i++)
+                for (int i = 0; i < PlayerSettingsController.NumberOfPlayerColors(); i++)
                 {
                     //If the taken color list does not contain the current color list, take it
                     if (!takenColors.Contains(i))
@@ -342,7 +338,7 @@ public class NetworkPlayer : MonoBehaviour
     {
         Debug.Log("Updating Taken Color List...");
 
-        if(ReadyUpManager.instance != null)
+        if (ReadyUpManager.instance != null)
         {
             //Refreshes the tubes
             if (FindObjectOfType<LockerTubeSpawner>() != null)
@@ -520,48 +516,6 @@ public class NetworkPlayer : MonoBehaviour
     public void RPC_Tether(int targetId)
     {
 
-    }
-
-    [PunRPC]
-    public void RPC_RemoteSpawnPlayer(int tubeNumber)
-    {
-        lastTubeNumber = tubeNumber;
-        if (SpawnManager2.instance != null)
-        {
-            SpawnManager2.instance.MoveDemoPlayerToSpawnPoint(tubeNumber);
-        }
-    }
-    [PunRPC]
-    public void RPC_TubeVacated(int tubeNumber)
-    {
-        if (SpawnManager2.instance != null)
-        {
-            LockerTubeController tube = FindObjectOfType<LockerTubeSpawner>().GetTubeByIndex(tubeNumber);
-            tube.UpdateLights(false);
-            Debug.Log("TestTube" + tubeNumber + " Is Being Vacated...");
-        }
-        if (ReadyUpManager.instance != null)
-        {
-            ReadyUpManager.instance.OnLeftRoom();
-            ReadyUpManager.instance.UpdateReadyText();
-        }
-        
-    }
-
-    //BELOW METHODS ONLY GET CALLED ON MASTER CLIENT
-    [PunRPC]
-    public void RPC_GiveMeSpawnpoint(int myViewID)
-    {
-        if (SpawnManager2.instance != null && !inTube)
-        {
-            LockerTubeController spawnTube = SpawnManager2.instance.GetEmptyTube();
-            if (spawnTube != null)
-            {
-                Player targetPlayer = PhotonNetwork.GetPhotonView(myViewID).Owner;
-                photonView.RPC("RPC_RemoteSpawnPlayer", targetPlayer, spawnTube.GetTubeNumber());
-                inTube = true;
-            }
-        }
     }
 
     //UTILITY METHODS:
