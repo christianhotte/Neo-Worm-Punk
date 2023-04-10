3cb07c38952ac286911a79dbcfd49c0b33f43d13
pgomesProjects <70992905+pgomesProjects@users.noreply.github.com>
2023-04-10 11:45:12 -0400

    Working On Inverteboy

--- Assets/Scripts/UI/PlayerHUD/Inverteboy.cs
+++ Assets/Scripts/UI/PlayerHUD/Inverteboy.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Inverteboy : MonoBehaviour
 {
+    public enum InverteboyScreens { MAIN, ARENA, TUTORIAL, VISUALIZER, MENU }
+
     private AudioSource audioSource;
+    private Transform wristTransform;
+    [SerializeField, Tooltip("The label text.")] private TextMeshProUGUI labelText;
+    [SerializeField, Tooltip("The tutorial text.")] private TextMeshProUGUI tutorialText;
+
+    [SerializeField, Tooltip("The list of the different menus on the inverteboy.")] private Canvas[] inverteboyCanvases;
+
+    private Canvas currentCanvas;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        currentCanvas = inverteboyCanvases[(int)InverteboyScreens.MAIN];
         UpdateVolume();
     }
 
@@ -20,6 +31,48 @@ public class Inverteboy : MonoBehaviour
     {
         audioSource.clip = audioClip;
         audioSource.Play();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Updates the tutorial menu text.
+    /// </summary>
+    /// <param name="message">The message for the tutorial.</param>
+    /// <param name="label">The label of the tutorial.</param>
+    public void UpdateTutorialText(string message, string label = "")
+    {
+        labelText.text = label;
+        tutorialText.text = message;
+    }
+
+    /// <summary>
+    /// Switches the canvas of the Inverteboy.
+    /// </summary>
+    /// <param name="canvasIndex">the index of the new canvas.</param>
+    public void SwitchCanvas(int canvasIndex)
+    {
+        Canvas newCanvas = inverteboyCanvases[canvasIndex];
+
+        currentCanvas.enabled = false;
+        currentCanvas = newCanvas;
+        currentCanvas.enabled = true;
+    }
+
+    /// <summary>
+    /// Switches the canvas of the Inverteboy.
+    /// </summary>
+    /// <param name="canvasIndex">the index of the new canvas.</param>
+    public void SwitchCanvas(InverteboyScreens canvasIndex)
+    {
+        Canvas newCanvas = inverteboyCanvases[(int)canvasIndex];
+
+        currentCanvas.enabled = false;
+        currentCanvas = newCanvas;
+        currentCanvas.enabled = true;
     }
 
     public void StopMusic() => audioSource.Stop();
