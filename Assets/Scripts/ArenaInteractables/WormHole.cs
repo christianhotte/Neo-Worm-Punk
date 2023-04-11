2c5d405c78a934627250e83c4f161cf490805c3f
AlmostcoolFSU <44479998+AlmostcoolFSU@users.noreply.github.com>
2023-04-11 10:56:45 -0400

    Fixed sounds playing over eachother

--- Assets/Scripts/ArenaInteractables/WormHole.cs
+++ Assets/Scripts/ArenaInteractables/WormHole.cs
@@ -108,7 +108,7 @@ public class WormHole : NetworkedArenaElement
 
     public IEnumerator TimedLock()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(10.0f);
         locked = false;
     }
 }
