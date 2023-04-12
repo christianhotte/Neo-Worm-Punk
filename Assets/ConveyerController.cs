288959de0cf479f36ad0672d83eea9b6cf120890
Dmarsh12fitch <dmarsh12@student.fitchburgstate.edu>
2023-04-12 11:39:07 -0400

    added movement to conveyer controller

--- Assets/ConveyerController.cs
+++ Assets/ConveyerController.cs
@@ -6,6 +6,7 @@ public class ConveyerController : MonoBehaviour
 {
     [SerializeField, Tooltip("teehee this is a fake tooltip")] private Transform[] conveyerBeltStopPositions;
     [SerializeField, Tooltip("")] private Transform[] conveyerBeltObjects;
+    [SerializeField, Tooltip("How much time it takes for objects to get from conveyerbelt stop positions")] private float transportTime;
 
     private int currentConveyerBeltIndex = -1;
     private bool conveyerBeltIsMoving = false;
@@ -35,20 +36,50 @@ public class ConveyerController : MonoBehaviour
     {
         conveyerBeltIsMoving = true;
 
-        yield return new WaitForSeconds(1);
-        //do actual moving here instead of teleporting
+        //wait a tiny bit
+        yield return new WaitForSeconds(0.5f);                          //do other things in this time like retract stuffs
+
+        //make arrays to store starting + ending positions
+        Vector3[] startPositions = new Vector3[conveyerBeltObjects.Length];
+        Vector3[] endPositions = new Vector3[conveyerBeltObjects.Length];
+
+        //assign the arrays wit the correct starting and ending positions
         for(int i = 0; i < conveyerBeltObjects.Length; i++)
         {
-            conveyerBeltObjects[i].position = new Vector3(conveyerBeltObjects[i].position.x, conveyerBeltObjects[i].position.y, conveyerBeltStopPositions[nextConveyerBeltIndex].position.z);
+            startPositions[i] = conveyerBeltObjects[i].position;
+            endPositions[i] = new Vector3(conveyerBeltObjects[i].position.x, conveyerBeltObjects[i].position.y, conveyerBeltStopPositions[nextConveyerBeltIndex].position.z);
+        }
+        
+
+        //set initial time to 0
+        float timeElapsed = 0;
+
+        //lerp the objects from start to end positions
+        while (timeElapsed < transportTime)
+        {
+            Debug.Log("START MOVING, timeElapsed = " + timeElapsed);
+            //smooth lerp duration alg
+            float t = timeElapsed / transportTime;
+            t = t * t * (3f - 2f * t);
+
+            //loops through all of the conveyer belt objects and lerps them from start to end
+            for(int i = 0; i < conveyerBeltObjects.Length; i++)
+            {
+                conveyerBeltObjects[i].position = Vector3.Lerp(startPositions[i], endPositions[i], t);
+                //conveyerBeltObjects[i].position = endPositions[i];
+            }
+
+            //advance time
+            timeElapsed += Time.deltaTime;
+            //timeElapsed = 100;
+
+            //just in case
+            if(GameManager.Instance.levelTransitionActive) { break; }
+
+            yield return null;
         }
 
-        //if(conveyerBeltObjects.Length == 2)
-        //{
-            //do thing also for the plat
-        //}
-
-
-        currentConveyerBeltIndex++;
+        currentConveyerBeltIndex = nextConveyerBeltIndex;
         conveyerBeltIsMoving = false;
     }
     
