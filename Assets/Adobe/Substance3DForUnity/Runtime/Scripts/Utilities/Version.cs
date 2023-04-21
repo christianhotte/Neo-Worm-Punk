f4cfb45392081a4d1340a50a517cf962d5460747
Christian <cgulick@student.fitchburgstate.edu>
2023-04-21 08:59:20 -0400

    Revert "Merge branch 'cgulick' into working"
    This reverts commit fecf7401f65a39cf651ab5ecb60e935a169024aa, reversing
    changes made to a08fb88113396f69d20c9331e0a875ad1a39d784.

--- Assets/Adobe/Substance3DForUnity/Runtime/Scripts/Utilities/Version.cs
+++
@@ -1,9 +0,0 @@
-namespace Adobe.Substance
-{
-    public static class Version
-    {
-        public const string PluginVersion = "3.6.0-5-gfab1c7f";
-        public const string EngineVersion = "8.2.0 0138644d";
-        public const int ImporterVersion = 360;
-    }
-}
