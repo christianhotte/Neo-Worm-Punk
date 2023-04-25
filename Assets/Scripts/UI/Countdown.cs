0f37e66c8083ba37ddd868b5c746b250c1297fbb
pgomesProjects <70992905+pgomesProjects@users.noreply.github.com>
2023-04-25 15:21:45 -0400

    Countdown Script Created

---
+++ Assets/Scripts/UI/Countdown.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField] private GameObject roomSettings;
+    [SerializeField] private GameObject countdownContainer;
+    [SerializeField] private TextMeshProUGUI countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
