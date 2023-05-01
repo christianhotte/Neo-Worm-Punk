1a0d9c939cd7f397c43e2a212b383044c4f9b4ff
AlmostcoolFSU <44479998+AlmostcoolFSU@users.noreply.github.com>
2023-05-01 14:26:09 -0400

    grinder death fountain :)

--- Assets/Scripts/ArenaInteractables/Grinder.cs
+++ Assets/Scripts/ArenaInteractables/Grinder.cs
@@ -7,21 +7,25 @@ using UnityEngine.SceneManagement;
 
 public class Grinder : MonoBehaviour
 {
+    public static Grinder instance;
     private PlayerController hitPlayer;
     public GameObject leftDoorStart, rightDoorStart, leftDoorEnd, rightDoorEnd;
     private NetworkPlayer netPlayer;
-    public bool Activated = false, Closed = true,Enabled=true;
+    public bool Activated = false, Closed = true, Enabled = true;
     public float doorSpeed = 3;
     public float LevelTimePercent;
     internal AudioSource GrinderAud;
     public AudioClip GrindnerSound;
     //private Jumbotron jumbotronObject;
     private RoundManager roundManager;
+    [Header("Effects:")]
+    public ParticleSystem deathEffect;
 
     // Start is called before the first frame update
     void Awake()
     {
         //jumbotronObject = FindObjectOfType<Jumbotron>();
+        instance = this;
         GrinderAud = this.GetComponent<AudioSource>();
         PhotonView masterPV = PhotonView.Find(17);
         roundManager = masterPV.GetComponent<RoundManager>();
@@ -30,7 +34,7 @@ public class Grinder : MonoBehaviour
     public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         //check to see if activated in room setting
-        Enabled=(bool)PhotonNetwork.CurrentRoom.CustomProperties["HazardsActive"];
+        Enabled = (bool)PhotonNetwork.CurrentRoom.CustomProperties["HazardsActive"];
 
 
     }
@@ -40,30 +44,35 @@ public class Grinder : MonoBehaviour
         if (Activated && Closed)
         {
             rightDoorStart.transform.position = Vector3.MoveTowards(rightDoorStart.transform.position, rightDoorEnd.transform.position, doorSpeed);
-            leftDoorStart.transform.position = Vector3.MoveTowards(leftDoorStart.transform.position,leftDoorEnd.transform.position,doorSpeed);
+            leftDoorStart.transform.position = Vector3.MoveTowards(leftDoorStart.transform.position, leftDoorEnd.transform.position, doorSpeed);
         }
         if (leftDoorStart.transform.position == leftDoorEnd.transform.position)
         {
             Closed = true;
         }
-        if (!Activated && Closed&&Enabled)
+        if (!Activated && Closed && Enabled)
         {
             //LevelTimePercent = jumbotronObject.GetLevelTimer().LevelTimePercentage();
             LevelTimePercent = roundManager.LevelTimePercentage();
             if (LevelTimePercent >= 60)
             {
                 Activated = true;
-                if(GrindnerSound!=null)GrinderAud.PlayOneShot(GrindnerSound);
+                if (GrindnerSound != null) GrinderAud.PlayOneShot(GrindnerSound);
             }
         }
     }
     private void OnTriggerStay(Collider other)
     {
-        if (other.name == "XR Origin"&& Activated&&Enabled)
+        if (other.name == "XR Origin" && Activated && Enabled)
         {
             netPlayer = PlayerController.photonView.GetComponent<NetworkPlayer>();
             netPlayer.photonView.RPC("RPC_Hit", RpcTarget.All, 100, netPlayer.photonView.ViewID, Vector3.zero, (int)DeathCause.TRAP);
+            netPlayer.photonView.RPC("RPC_TriggerEffect", RpcTarget.All, 0);
         }
     }
-
+    public void PlayDeathEffect()
+    {
+        if (deathEffect == null) return;
+        deathEffect.Play(true);
+    }
 }
